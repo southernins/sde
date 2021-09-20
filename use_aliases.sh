c156e350874341eb74bf0945fd0f92323baa9ca7
ncrewsSouthern <ncrews@southerninsurance.net>
2021-09-19 20:07:45 -0500

    mac aliases

--- use_aliases.sh
+++ use_aliases.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Bash shell "typicall" loads .bash_aliases out of the box
+# note "typically"  please verify this before use.
 if [ -r ~/.bashrc ]; then
 
   # move aliases file to home directory
@@ -7,4 +9,16 @@ if [ -r ~/.bashrc ]; then
 
   # source the alias file for the current session
   . ~/.bash_aliases
+fi
+
+
+# Mac OS Big Sur uses zsh instead of bash.
+# you need to add loading of .zsh_aliases in .zshrc for this to work
+if [ -r ~/.zshrc ]; then
+
+  # move aliases file to home directory
+  cp -i ./aliases/.bash_aliases ~/.zsh_aliases
+
+  # source the alias file for the current session
+  . ~/.zsh_aliases
 fi
